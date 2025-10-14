d2fc2904733109a3bcc92fd12138dabe3ce58d00
John Cameron <john@cameronfoundation.org>
2025-10-14 18:40:58 +1100

    Added dummy skill

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ def load_all_skills() -> List[str]:
   # populated from ESCO.
   # Could be around 20,000 of these
   return [
-    "Java", "Spring Boot", "Python", "FastAPI", "Docker", "Kubernetes", "PostgreSQL",
+    "Java", "Spring Boot", "Python", "FastAPI", "Docker", "C++", "Kubernetes", "PostgreSQL",
     "MapStruct", "Angular", "AWS", "Terraform", "Natural language processing","Spring"
   ]
 
