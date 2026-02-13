bfa8e61a12dfb993637315bc8c1ee526941ee883
sadatmalik <sadat.malik@me.com>
2026-02-13 17:15:32 +0000

    refactor ACM certificate configuration by removing Route53 DNS validation resources

--- infra/terraform/acm.tf
+++ infra/terraform/acm.tf
@@ -1,12 +1,6 @@
 ################################################################################
-# ACM Certificate and Route53 DNS Validation
+# ACM Certificate
 ################################################################################
-
-resource "aws_route53_zone" "this" {
-  name = var.site_domain
-
-  tags = local.tags
-}
 
 resource "aws_acm_certificate" "this" {
   domain_name       = var.site_domain
@@ -19,22 +13,7 @@ resource "aws_acm_certificate" "this" {
   tags = local.tags
 }
 
-resource "aws_route53_record" "certificate_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      type   = dvo.resource_record_type
-      record = dvo.resource_record_value
-    }
-  }
-
-  zone_id = aws_route53_zone.this.zone_id
-  name    = each.value.name
-  type    = each.value.type
-  ttl     = 60
-  records = [each.value.record]
-}
-
+# Waits for DNS validation to complete before the certificate is usable
 resource "aws_acm_certificate_validation" "this" {
   certificate_arn         = aws_acm_certificate.this.arn
   validation_record_fqdns = [for r in aws_route53_record.certificate_validation : r.fqdn]
