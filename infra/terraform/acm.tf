d5e03ae3f394c342387ff97b2ce1c921d6f89ecc
sadatmalik <sadat.malik@me.com>
2026-02-13 11:30:12 +0000

    add ACM certificate and Route53 DNS validation resources for site domain

---
+++ infra/terraform/acm.tf
@@ -0,0 +1,41 @@
+################################################################################
+# ACM Certificate and Route53 DNS Validation
+################################################################################
+
+resource "aws_route53_zone" "this" {
+  name = var.site_domain
+
+  tags = local.tags
+}
+
+resource "aws_acm_certificate" "this" {
+  domain_name       = var.site_domain
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = local.tags
+}
+
+resource "aws_route53_record" "certificate_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = aws_route53_zone.this.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+}
+
+resource "aws_acm_certificate_validation" "this" {
+  certificate_arn         = aws_acm_certificate.this.arn
+  validation_record_fqdns = [for r in aws_route53_record.certificate_validation : r.fqdn]
+}
